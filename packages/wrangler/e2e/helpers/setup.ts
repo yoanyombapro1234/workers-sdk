93d6bd5262c2476cd545db7d942c74e2c99aff7c
bcoll <bcoll@cloudflare.com>
2023-11-22 12:11:30 +0000

    Implement additional E2E tests for `wrangler dev (--remote)`

--- packages/wrangler/e2e/helpers/setup.ts
+++ packages/wrangler/e2e/helpers/setup.ts
@@ -46,7 +46,10 @@ export function dedent(strings: TemplateStringsArray, ...values: unknown[]) {
 
 // Seeds the `root` directory on the file system with some data. Use in
 // combination with `dedent` for petty formatting of seeded contents.
-export async function seed(root: string, files: Record<string, string>) {
+export async function seed(
+	root: string,
+	files: Record<string, string | Uint8Array>
+) {
 	// TODO(someday): allow copying/symlinking file/directory paths in seed? like "path`${__dirname}/../fixture`"?
 	for (const [name, contents] of Object.entries(files)) {
 		const filePath = path.resolve(root, name);
