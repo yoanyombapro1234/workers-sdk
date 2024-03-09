a39710333da6ebb12f3fe375c92a5b28d05c7f98
Samuel Macleod <smacleod@cloudflare.com>
2024-03-09 00:48:00 +0000

    more test fixups

--- fixtures/local-mode-tests/tests/logging.test.ts
+++ fixtures/local-mode-tests/tests/logging.test.ts
@@ -30,25 +30,5 @@ it("logs startup errors", async () => {
 		await worker.stop();
 		expect.fail("Expected unstable_dev() to fail");
 	} catch {}
-	expect(output).toMatchInlineSnapshot(`
-		"[31m✘ [41;31m[[41;97mERROR[41;31m][0m [1mservice core:user:local-mode-tests: Uncaught Error: No such module "node:buffer".[0m
-
-		    imported from "nodejs-compat.js"
-
-
-		[31m✘ [41;31m[[41;97mERROR[41;31m][0m [1mError reloading local server: MiniflareCoreError [ERR_RUNTIME_FAILURE]: The Workers runtime failed to start. There is likely additional logging output above.[0m
-
-		      at Miniflare.#assembleAndUpdateConfig (/Users/penalosa/dev/wrangler2/packages/miniflare/src/index.ts:1304:10)
-		      at processTicksAndRejections (node:internal/process/task_queues:96:5)
-		      at Mutex.runWith (/Users/penalosa/dev/wrangler2/packages/miniflare/src/workers/shared/sync.ts:66:45)
-		      at Miniflare.#waitForReady (/Users/penalosa/dev/wrangler2/packages/miniflare/src/index.ts:1386:3)
-		      at EventTarget.#onBundleUpdate (/Users/penalosa/dev/wrangler2/packages/wrangler/import_meta_url.js:109246:20)
-		      at Mutex.runWith (/Users/penalosa/dev/wrangler2/packages/miniflare/src/workers/shared/sync.ts:66:45) {
-		    code: 'ERR_RUNTIME_FAILURE',
-		    cause: undefined
-		  }
-
-
-		"
-	`);
+	expect(output.split("\n")[0]).toMatchInlineSnapshot(`"[31m✘ [41;31m[[41;97mERROR[41;31m][0m [1mservice core:user:local-mode-tests: Uncaught Error: No such module "node:buffer".[0m"`);
 });
