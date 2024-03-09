dd3e0ceab1bf0e96b7223a69d0f37964d6aa89bd
Samuel Macleod <smacleod@cloudflare.com>
2024-03-09 01:15:18 +0000

    more test fixups

--- fixtures/local-mode-tests/tests/logging.test.ts
+++ fixtures/local-mode-tests/tests/logging.test.ts
@@ -30,7 +30,5 @@ it("logs startup errors", async () => {
 		await worker.stop();
 		expect.fail("Expected unstable_dev() to fail");
 	} catch {}
-	expect(output.split("\n")[0].trim()).toMatchInlineSnapshot(
-		`"[31m✘ [41;31m[[41;97mERROR[41;31m][0m [1mservice core:user:local-mode-tests: Uncaught Error: No such module "node:buffer".[0m"`
-	);
+	expect(output).toContain(`No such module "node:buffer"`);
 });
