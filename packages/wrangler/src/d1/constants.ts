7b73637601fd22f096d50fe8d7cbab5272034a18
Glen Maddern <glen@cloudflare.com>
2024-04-16 19:41:26 +1000

    Adds R2-based SQL import into remote D1 DBs
    This hits a new API endpoint 'import', that initially returns a signed R2 PUT url for uploading SQL, then returns polling info as the import completes. This completely bypasses the existing split-chunk-and-execute implementation that often left your DB in an invalid state

--- packages/wrangler/src/d1/constants.ts
+++ packages/wrangler/src/d1/constants.ts
@@ -1,4 +1,12 @@
 export const DEFAULT_MIGRATION_PATH = "./migrations";
 export const DEFAULT_MIGRATION_TABLE = "d1_migrations";
-export const LOCATION_CHOICES = ["weur", "eeur", "apac", "oc", "wnam", "enam"];
+export const LOCATION_CHOICES = [
+	"weur",
+	"eeur",
+	"apac",
+	"oc",
+	"wnam",
+	"enam",
+	"eagle",
+];
 // Max number of statements to send in a single /execute call
